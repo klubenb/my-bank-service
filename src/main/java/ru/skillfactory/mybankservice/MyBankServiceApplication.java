0307cfc7a9486a2b311a6965ce6ec270007061ff
Anton Kartashov <anton.kartashov@raiffeisen.ru>
2024-07-16 19:25:30 +0300

    Initial commit

---
+++ src/main/java/ru/skillfactory/mybankservice/MyBankServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.skillfactory.mybankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBankServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skillfactory/mybankservice/MyBankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skillfactory.mybankservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
