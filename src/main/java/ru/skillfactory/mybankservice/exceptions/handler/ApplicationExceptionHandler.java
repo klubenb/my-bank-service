dcb7e01d10740f4533ad0623af989310b703de6c
Anton Kartashov <anton.kartashov@raiffeisen.ru>
2024-07-30 22:04:24 +0300

    Add RecordNotFoundException to handler

--- src/main/java/ru/skillfactory/mybankservice/exceptions/handler/ApplicationExceptionHandler.java
+++ src/main/java/ru/skillfactory/mybankservice/exceptions/handler/ApplicationExceptionHandler.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import ru.skillfactory.mybankservice.api.dto.BaseResponseDto;
+import ru.skillfactory.mybankservice.api.dto.account.BaseResponseDto;
+import ru.skillfactory.mybankservice.exceptions.RecordNotFoundException;
 
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
@@ -12,6 +13,12 @@ public class ApplicationExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
     public BaseResponseDto handleIllegalArgumentException(IllegalArgumentException ex) {
+        return BaseResponseDto.asFailure(ex.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(RecordNotFoundException.class)
+    public BaseResponseDto handleRecordNotFoundException(RecordNotFoundException ex) {
         return BaseResponseDto.asFailure(ex.getMessage());
     }
 }
