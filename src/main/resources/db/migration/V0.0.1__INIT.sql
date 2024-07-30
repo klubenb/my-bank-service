daf0b0b0419fcf65ad3b703e8e31a8f03d6f2c6a
Anton Kartashov <anton.kartashov@raiffeisen.ru>
2024-07-30 20:29:31 +0300

    Refactor db.migration

--- src/main/resources/db/migration/V0.0.1__INIT.sql
+++ src/main/resources/db/migration/V0.0.1__INIT.sql
@@ -1,18 +1,9 @@
 -- ********************* account *********************
 CREATE TABLE IF NOT EXISTS account
-(
-    id
-    UUID
-    DEFAULT
-    gen_random_uuid
-(
-) PRIMARY KEY,
-    balance MONEY DEFAULT 0.0 NOT NULL
+(   id          UUID        DEFAULT gen_random_uuid() PRIMARY KEY,
+    balance     MONEY       DEFAULT 0.0 NOT NULL
     );
 
-COMMENT
-ON TABLE account IS 'Таблица счетов';
-COMMENT
-ON COLUMN account.id IS 'Идентификатор клиента';
-COMMENT
-ON COLUMN account.balance IS 'Баланс клиента';
+COMMENT ON TABLE account IS 'Таблица счетов';
+COMMENT ON COLUMN account.id IS 'Идентификатор клиента';
+COMMENT ON COLUMN account.balance IS 'Баланс клиента';
